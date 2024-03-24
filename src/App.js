caa6ef8a9891eceb0c06e235d3c3062f0ffffddc
hamza2912 <hamzaali2912@gmail.com>
2024-03-24 05:34:04 -0700

    chatbot ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-// import Header from './components/header';
-// import Footer from './components/footer';
 import './App.css';
-import './firebase';
 
 function App() {
   return (
-    <BrowserRouter>
-
-    {/* <Header/> */}
 
     <Routes>
         <Route path="/" element={<Home />}></Route>
     </Routes>
 
-    {/* <Footer /> */}
-
-    </BrowserRouter>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,82 +1,76 @@
 import React from 'react';
-import { getDatabase, ref, set, onValue, push } from "firebase/database";
+import axios from 'axios';
 
 function Home() {
 
-  const [activeChat, setactiveChat] = React.useState({});
-  const [showChat, setshowChat] = React.useState(false);
-  const [chats, setChats] = React.useState([]);
-  const [messages, setMessages] = React.useState({});
+  const [messages, setMessages] = React.useState([]);
   const [message, setmessage] = React.useState('');
-  const [messageIsAdded, setmessageIsAdded] = React.useState(false);
-  const db = getDatabase(); 
   const bottomE = React.useRef(null);
-  const [userId, setuserId] = React.useState('hamza2912');
 
   
   const scrollToBottom = () => {
-    bottomE.current?.scrollIntoView({ behavior: 'smooth' });
+    bottomE.current?.scrollIntoView({ behavior: 'smooth', block: 'center'});
   };
   
-  React.useEffect(() => {
-    // firebase
-    // var chats = []
-    // const user = ref(db, 'users/' + userId);
-    // onValue(user, (snapshot) => {
-    //     if(snapshot.val()){
-    //       chats = snapshot.val().mychats;
-    //       const mychat = ref(db, 'chatroom/' + chats[0]);
-    //       onValue(mychat, (snapshot) => {
-    //         if(snapshot.val()){
-    //           setMessages(snapshot.val().messages)
-    //         }
-    //       });
-    //     }
-    // });
-    // console.log(messages);
-    
-    
-    // set(ref(db, 'users/' + userId), {
-    //   username: userId,
-    //   name: 'Hamza',
-    //   profile_picture : 'H',
-    //   mychats : ['chat-1']
-    // });
 
-    // set(ref(db, 'chatroom/chat-1'), {
-    //   date: '2023-8-26',
-    //   messages : {
-         
-    //   }
-    // });
-    
-    // listener
+  React.useEffect(() => {
     const listener = event => {
       if (event.code === "Enter" || event.code === "NumpadEnter") {
-        addtoChat();
+        addtoChat(sendToRasa);
       }
     };
     document.addEventListener("keydown", listener);
     return () => {
       document.removeEventListener("keydown", listener);
     };
-  },[messageIsAdded]);
+  },[message, messages]);
 
-  function addtoChat(){
-    console.log(message);
-    const getMessages = ref(db, 'chatroom/chat-1/messages');
-    const appendToMessages = push(getMessages);
-    set(appendToMessages, {sender: userId, message: message});
-    // setChats(prevState => [...prevState, {type: "sender", message: message}])
+
+  function addtoChat(myCallback){
+
+    let appendToMessages = [...messages, {
+      "sender": 'me',
+      "message": message
+    }];
+    setMessages(appendToMessages);
     setmessage('');
     scrollToBottom();
-    setmessageIsAdded(!messageIsAdded)
+    myCallback(appendToMessages);   
+
   }
+  
+
+  function sendToRasa(old_chat){
+
+    axios.post(' http://localhost:5005/webhooks/rest/webhook', {
+      "sender": 'me',
+      "message": message
+    })
+    .then(function (response) {
+      response.data?.map( msg=> {
+        console.log(msg);
+        if(msg.recipient_id == 'me'){
+          var chats = [...old_chat, {
+            "sender": 'bot',
+            "message": msg.text,
+            "imgurl": msg.image
+          }]
+          setMessages(chats);
+          old_chat = [...chats];
+        }
+      })
+      scrollToBottom();
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
 
   return (
       
     <div className='w-full h-screen'>
-      <div className='chatbox w-1/4 fixed right-0 bottom-0 h-2/3 shadow-2xl text-white'>
+      <div className='chatbox w-1/3 fixed right-0 bottom-0 h-2/3 shadow-2xl text-white'>
         
         {/* <div className='chat-head bg-gray-900 p-5'>
           <h1>ChatRoom</h1>
@@ -96,21 +90,22 @@ function Home() {
             <div className='px-5 py-3 bg-white rounded-full text-black'>
               <h1>A</h1>
             </div>
-            <p className=''>Aziz</p>
+            <p className=''>Chatbot</p>
         </div>
         <div className='chat-body h-4/5 relative'>
           <div className='h-full p-2 overflow-y-auto'>
-            {/* <ul className='w-full grid grid-cols-1 gap-2'>
+            <ul className='w-full grid grid-cols-1 gap-2'>
               {
                 messages.map(chat=>{
                   return (
-                    <li className={'w-full flex ' + (chat.sender == userId ? 'justify-start' : 'justify-end')}>
-                      <p className={'w-1/2 px-4 py-2 rounded-lg ' + (chat.sender == userId ? 'bg-gray-900' : 'bg-blue-400')}>{chat.message}</p>
+                    <li className={'w-full flex ' + (chat.sender == 'me' ? 'justify-start' : 'justify-end')}>
+                      {chat.message?<p className={'w-1/2 px-4 py-2 rounded-lg ' + (chat.sender == 'me' ? 'bg-gray-900' : 'bg-blue-400')}>{chat.message}</p>
+                      :<img className='w-1/2 h-32 object-fit rounded-lg' src={`${chat.imgurl}`} />}
                     </li>
                   )
                 })
               }
-            </ul> */}
+            </ul>
             <div className='h-24' ref={bottomE}></div>
             <div className='w-full bg-white absolute bottom-0 left-0 p-2 flex gap-2 justify-around'>
               <input value={message} onChange={(event)=>setmessage(event.target.value)} className='w-4/5 rounded-md px-2 py-1 bg-gray-200 focus:outline-none text-black' type="text" placeholder='Enter your message' />
